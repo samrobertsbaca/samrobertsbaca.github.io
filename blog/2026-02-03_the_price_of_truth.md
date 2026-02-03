900ba848d0935803356f17255bb39a1b5e43d776
Samuel Roberts-Baca <samrobertsbaca@gmail.com>
2026-02-03 09:35:45 -0700

    price of truth

---
+++ blog/2026-02-03_the_price_of_truth.md
@@ -0,0 +1,6 @@
+Feb 3
+
+# the price of truth
+
+the price of truth is love on all sides.  
+let go and expand.  
--- blog/blogposts.js
+++ blog/blogposts.js
@@ -1,4 +1,5 @@
 window.postFiles = [
+  "/blog/2026-02-03_the_price_of_truth.html",
   "/blog/2026-02-01_the_price_of_peace.html",
   "/blog/2026-01-30_always_by_your_side.html",
   "/blog/2026-01-28_a_universe_within.html",
