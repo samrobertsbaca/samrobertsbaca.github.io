071ff89488a30fb6a9b86ccd69763f3b92fefa55
Samuel Roberts-Baca <samrobertsbaca@gmail.com>
2026-04-24 04:01:44 -0600

    update

--- blog/2026-04-24_today_i_made_a_lemon_pig.md
+++ blog/2026-04-24_today_i_made_a_lemon_pig.md
@@ -2,4 +2,4 @@ Apr 24
 
 # today i made a lemon pig
 
-![may you be blessed with 318 quadrillion years of good luck... and then some](/images/lemonpig.png)
+![may you be blessed with 318 quadrillion years of good luck... and then some](/images/lemonpig_bigger.png)
--- exp/selfportrait/media.js
+++ exp/selfportrait/media.js
@@ -45,7 +45,7 @@ export const IMAGE_URLS = [
   "/images/izotope_freq_spec.png",
   "/images/kimchi.png",
   "/images/lemonpig.jpg",
-  "/images/lemonpig.png",
+  "/images/lemonpig_bigger.png",
   "/images/lumi_1.png",
   "/images/lumi_2.png",
   "/images/lumi_3.png",
